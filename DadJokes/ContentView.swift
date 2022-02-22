6c779898f4eab714764f8be29b9c1dca9050ee02
Russell Gordon <github@russellgordon.ca>
2022-02-22 11:41:10 -0500

    Laid out a static interface for the app.

--- DadJokes/ContentView.swift
+++ DadJokes/ContentView.swift
@@ -2,20 +2,61 @@
 //  ContentView.swift
 //  DadJokes
 //
-//  Created by Russell Gordon on 2022-02-22.
+//  Created by Russell Gordon on 2022-02-21.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            
+            Text("How do you organize a space party? You planet.")
+                .font(.title)
+                .multilineTextAlignment(.leading)
+                .padding(30)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 16)
+                        .stroke(Color.primary, lineWidth: 4)
+                )
+                .padding(10)
+            
+            Image(systemName: "heart.circle")
+                .font(.largeTitle)
+            
+            Button(action: {
+                print("I've been pressed.")
+            }, label: {
+                Text("Another one!")
+            })
+                .buttonStyle(.bordered)
+            
+            HStack {
+                Text("Favourites")
+                    .bold()
+                
+                Spacer()
+            }
+            
+            List {
+                Text("Which side of the chicken has more feathers? The outside.")
+                Text("Why did the Clydesdale give the pony a glass of water? Because he was a little horse!")
+                Text("The great thing about stationery shops is they're always in the same place...")
+            }
+            
+            Spacer()
+                        
+        }
+        .navigationTitle("icanhazdadjoke?")
+        .padding()
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView {
+            ContentView()
+        }
     }
 }
